c1cc226bbd19dd003c5bc39cfea9a28ed3e6da03
dannyzou18 <dzou3814@gmail.com>
2023-02-17 23:59:14 -0500

    deleted:    test/.DS_Store 	deleted:    test/README.txt 	deleted:    test/a.out 	deleted:    test/a.out.dSYM/Contents/Info.plist 	deleted:    test/a.out.dSYM/Contents/Resources/DWARF/a.out 	deleted:    test/customer.cpp 	deleted:    test/customer.h 	deleted:    test/customer_large.txt 	deleted:    test/customer_medium.txt 	deleted:    test/customer_small.txt 	deleted:    test/inventory.cpp 	deleted:    test/inventory.h 	deleted:    test/inventory_large.txt 	deleted:    test/inventory_medium.txt 	deleted:    test/inventory_small.txt 	deleted:    test/main.cpp 	deleted:    test/output_customer_large.txt 	deleted:    test/output_customer_medium.txt 	deleted:    test/output_customer_small.txt 	deleted:    test/output_inventory_large.txt 	deleted:    test/output_inventory_medium.txt 	deleted:    test/output_inventory_small.txt

--- test/inventory.cpp
+++
@@ -1,9 +0,0 @@
-#include "Inventory.h"
-
-Inventory::Inventory(const std::string &n, const std::string &i, const int q){
-    name = n;
-    id = i;
-    quantity = q;
-    //sets this at when it is created and never changes it
-    originalQuantity = q;
-}
